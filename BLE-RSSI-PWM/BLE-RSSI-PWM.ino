6f870aff96f68f9c17f1db23d978754e69b5082f
Michel Gutlich <mragutlich@gmail.com>
2025-11-29 09:51:25 +0100

    Update BLE-RSSI-PWM.ino

--- BLE-RSSI-PWM/BLE-RSSI-PWM.ino
+++ BLE-RSSI-PWM/BLE-RSSI-PWM.ino
@@ -1,6 +1,6 @@
 #include <BLEDevice.h>
 
-String targetMAC = "AA:BB:CC:DD:EE:FF";   // ← change to your BLE device MAC
+String targetMAC = "68:39:1a:62:54:84";   // ← change to your BLE device MAC
 int pwmPin = 5;                           // PWM output pin
 int pwmChannel = 0;
 int pwmFreq = 5000;
@@ -16,7 +16,8 @@ class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
       lastRSSI = rssi;
 
       Serial.print("Target RSSI: ");
-      Serial.println(rssi);
+      Serial.print(rssi);
+      Serial.print("  ,  ");
 
       // Convert RSSI (-100 to -30 dBm) → PWM (0 to 255)
       int pwmValue = map(rssi, -100, -30, 0, 255);
@@ -34,8 +35,8 @@ void setup() {
   Serial.begin(115200);
 
   // Setup PWM
-  ledcSetup(pwmChannel, pwmFreq, pwmRes);
-  ledcAttachPin(pwmPin, pwmChannel);
+  //ledcSetup(pwmChannel, pwmFreq, pwmRes);
+  //ledcAttachPin(pwmPin, pwmChannel);
 
   Serial.println("BLE RSSI Tracker starting...");
 
@@ -43,13 +44,13 @@ void setup() {
   BLEScan* scan = BLEDevice::getScan();
   scan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   scan->setActiveScan(true);
-  scan->setInterval(100);
-  scan->setWindow(80);
+  scan->setInterval(50);
+  scan->setWindow(40);
 }
 
 void loop() {
   BLEScan* scan = BLEDevice::getScan();
-  scan->start(2, false);   // Scan continuously (2-second cycles)
+  scan->start(1, false);   // Scan continuously (2-second cycles)
   scan->clearResults();
 
   // Timeout: If device not seen, fade PWM to 0
@@ -57,7 +58,7 @@ void loop() {
   if (lastRSSI > -100) lastSeen = millis();
 
   if (millis() - lastSeen > 3000) {
-    ledcWrite(pwmChannel, 0);
+    //ledcWrite(pwmChannel, 0);
     Serial.println("Device lost → PWM = 0");
   }
 }
